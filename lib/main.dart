39786740de10eff8f2b2576d2834cf908e554ed0
bgarcia95 <bgarcia.95@outlook.com>
2019-06-17 21:44:24 -0600

    My Own MiCard project finished on Flutter.

---
+++ lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                backgroundImage: AssetImage('images/cv.png'),
+                radius: 50.0,
+              ),
+              Text(
+                'Brayan García',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                'Flutter Developer',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  letterSpacing: 5.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Container(
+                width: 250.0,
+                child: Divider(
+                  color: Colors.teal[100],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Card(
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal[900],
+                    ),
+                    title: Text(
+                      '+503 7452-6878',
+                      style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 25.0),
+                child: Card(
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal[900],
+                    ),
+                    title: Text(
+                      'bgarcia.95@outlook.com',
+                      style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
